f89fa0261fd386ae5f91baed6f532e69d6654e8b
prof7bit <prof7bit@googlemail.com>
2010-12-26 16:17:27 +0000

    the long timeouts for dead connections were a bad idea

--- torchat/src/config.py
+++ torchat/src/config.py
@@ -61,8 +61,8 @@ config_defaults = {
 
 COPYRIGHT = u"Copyright (c) 2007-2010 Bernd Kreuß <prof7bit@gmail.com>"
 
-DEAD_CONNECTION_TIMEOUT = 360
-KEEPALIVE_INTERVAL = 120
+DEAD_CONNECTION_TIMEOUT = 90
+KEEPALIVE_INTERVAL = 60
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
 ICON_DIR = os.path.join(SCRIPT_DIR, "icons")
