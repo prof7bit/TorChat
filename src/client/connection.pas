fcc109e44243d253386b952076bf863cd9b353aa
prof7bit <prof7bit@googlemail.com>
2012-02-15 16:10:52 +0100

    add GPL3 license headers to all source files

--- src/client/connection.pas
+++ src/client/connection.pas
@@ -1,3 +1,22 @@
+{ TorChat - Connection class, representing a connection via Tor
+
+  Copyright (C) 2012 Bernd Kreuss <prof7bit@googlemail.com>
+
+  This source is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 3 of the License, or (at your option)
+  any later version.
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web
+  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
+}
 unit connection;
 
 {$mode objfpc}{$H+}
