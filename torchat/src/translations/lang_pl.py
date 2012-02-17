7284fe3ac2723310ec732baf64ab08b3e10453c7
Szary Czlowiek <szary.czlowiek@hotmail.com>
2012-02-17 16:04:20 +0100

    minor improvements of Polish translation

--- torchat/src/translations/lang_pl.py
+++ torchat/src/translations/lang_pl.py
@@ -11,7 +11,7 @@
 LANGUAGE_CODE = u"pl"
 LANGUAGE_NAME = u"Polski"
 LANGUAGE_NAME_ENGLISH = u"Polish"
-TRANSLATOR_NAMES = [u"yr3gwa7yonxrl26q"]
+TRANSLATOR_NAMES = [u"rmjy2avgeluvbrdq"]
 
 #buttons
 BTN_CANCEL = u"Anuluj"
@@ -36,15 +36,15 @@ MPOP_EDIT_CONTACT = u"Edytuj kontakt..."
 MPOP_DELETE_CONTACT = u"Usuń kontakt..."
 MPOP_SHOW_OFFLINE_MESSAGES = u"Pokaż wiadomości oczekujące"
 MPOP_CLEAR_OFFLINE_MESSAGES = u"Wyczyść wiadomości oczekujące"
-# MPOP_ACTIVATE_LOG = u"Activate logging to file"
-# MPOP_STOP_LOG = u"Stop logging"
-# MPOP_DELETE_EXISTING_LOG = u"Delete existing log file"
-# MPOP_DELETE_AND_STOP_LOG = u"Delete log and stop logging"
+MPOP_ACTIVATE_LOG = u"Aktywuj rejestrowanie do pliku"
+MPOP_STOP_LOG = u"Zatrzymaj rejestrowanie"
+MPOP_DELETE_EXISTING_LOG = u"Usuń istniejący plik dziennika"
+MPOP_DELETE_AND_STOP_LOG = u"Usuń plik dziennika i zatrzymaj rejestrowanie"
 MPOP_ADD_CONTACT = u"Dodaj kontakt..."
 MPOP_ABOUT = u"O programie TorChat"
 MPOP_ASK_AUTHOR = u"Zapytaj %s..."
 MPOP_SETTINGS = u"Ustawienia..."
-# MPOP_EDIT_MY_PROFILE = u"Edit my profile..."
+MPOP_EDIT_MY_PROFILE = u"Edytuj mój profil..."
 
 #chat window popup menu
 CPOP_COPY = u"Kopiuj"
@@ -57,9 +57,9 @@ D_CONFIRM_DELETE_MESSAGE = u"Czy na pewno chcesz usunać ?\n(%s %s)"
 D_LOG_WARNING_TITLE = u"TorChat: Rejestracja jest aktywna"
 D_LOG_WARNING_MESSAGE = u"Rejestracja została aktywowana!\n\nLog Plik: %s\n\nPamietaj usuń plik po zakończeniu pracy."
 
-# #warning about used port
-# D_WARN_USED_PORT_TITLE = u"TorChat: Port already in use"
-# D_WARN_USED_PORT_MESSAGE = u"Something, probably another TorChat instance, is already listening at %s:%s. You must create another profile using different ports to be able to start TorChat a second time."
+#warning about used port
+D_WARN_USED_PORT_TITLE = u"TorChat: Port w użyciu"
+D_WARN_USED_PORT_MESSAGE = u"Coś lub prawdopodobnie inna kopia TorChat jest w użyciu i nasłuchuje port na %s:%s. Musisz utworzyć inny profil używający innych portów aby uruchomić drugą kopię TorChat."
 
 #warnig about unread messages
 D_WARN_UNREAD_TITLE = u"TorChat: Nieprzeczytane wiadomosci"
@@ -73,13 +73,13 @@ D_WARN_BUDDY_OFFLINE_MESSAGE = u"Nie można wykonac tej operacji z kontaktami po
 D_WARN_FILE_ONLY_ONE_TITLE = u"TorChat: Kilka plików"
 D_WARN_FILE_ONLY_ONE_MESSAGE = u"Nie można wysłac kilku plików podczas jednej operacji. Wyślij każdy osobno lub użyj archiwum ZIP/RAR/7Z."
 
-# #warning about file save error
-# D_WARN_FILE_SAVE_ERROR_TITLE = u"TorChat: Error saving file"
-# D_WARN_FILE_SAVE_ERROR_MESSAGE = u"The file '%s' could not be created.\n\n%s"
+#warning about file save error
+D_WARN_FILE_SAVE_ERROR_TITLE = u"TorChat: Błąd podczas zapisywania pliku"
+D_WARN_FILE_SAVE_ERROR_MESSAGE = u"Plik '%s' nie mógł zostać utworzony.\n\n%s"
 
-# #warning about file already exists
-# D_WARN_FILE_ALREADY_EXISTS_TITLE = u"TorChat: File exists"
-# D_WARN_FILE_ALREADY_EXISTS_MESSAGE = u"The file '%s' already exists.\nOverwrite it?"
+#warning about file already exists
+D_WARN_FILE_ALREADY_EXISTS_TITLE = u"TorChat: Plik już istnieje"
+D_WARN_FILE_ALREADY_EXISTS_MESSAGE = u"Plik '%s' już istnieje.\nNadpisać go?"
 
 #dialog: add/edit contact
 DEC_TITLE_ADD = u"Dodaj nowy kontakt"
@@ -91,29 +91,29 @@ DEC_MSG_16_CHARACTERS = u"Adres musi mieć 16 znaków, nie %i."
 DEC_MSG_ONLY_ALPANUM = u"Adres musi zawierać tylko małe litery i cyfry"
 DEC_MSG_ALREADY_ON_LIST = u"%s już jest na liście"
 
-# #dialog: edit my profile
-# DEP_TITLE = u"Edit my profile"
-# DEP_NAME = u"Name"
-# DEP_TEXT = u"Text"
-# DEP_SET_AVATAR = u"Set Avatar"
-# DEP_REMOVE_AVATAR = u"Remove Avatar"
-# DEP_AVATAR_SELECT_PNG = u"Select .PNG file to use as your avatar (will be scaled to 64*64, may contain transparency)"
-# DEP_PNG_FILES = u"PNG files"
-# DEP_ALL_FILES = u"All files"
-# DEP_WARN_TITLE = u"Avatar selection not possible"
-# DEP_WARN_IS_ALREADY = u"This is already the current avatar"
-# DEP_WARN_MUST_BE_PNG = u"The avatar must be a .png file"
+#dialog: edit my profile
+DEP_TITLE = u"Edytuj mój profil"
+DEP_NAME = u"Nazwa"
+DEP_TEXT = u"Tekst"
+DEP_SET_AVATAR = u"Ustaw Avatar"
+DEP_REMOVE_AVATAR = u"Usuń Avatar"
+DEP_AVATAR_SELECT_PNG = u"Zaznać plik .PNG aby użyć go jako avatara (zostanie zmniejszony do 64*64)"
+DEP_PNG_FILES = u"Pliki PNG"
+DEP_ALL_FILES = u"Wszystkie pliki"
+DEP_WARN_TITLE = u"Zaznaczenie avatara nie jest możliwe"
+DEP_WARN_IS_ALREADY = u"To jest już aktualny avatar"
+DEP_WARN_MUST_BE_PNG = u"Avatar musi być plikiem .png"
 
 #file transfer window
 DFT_FILE_OPEN_TITLE = u"Wyślij plik do %s"
 DFT_FILE_SAVE_TITLE = u"Zapisz plik od %s"
 DFT_SEND = u"Wysłano %s\ndo %s\n%04.1f%% (%i z %i bajtów)"
 DFT_RECEIVE = u"Otrzymano %s\nod %s\n%04.1f%% (%i z %i bajtów)"
-# DFT_WAITING = u"waiting for connection"
-# DFT_STARTING = u"starting transfer"
-# DFT_ABORTED = u"transfer aborted"
-# DFT_COMPLETE = u"transfer complete"
-# DFT_ERROR = u"error"
+DFT_WAITING = u"oczekiwanie na połączenie"
+DFT_STARTING = u"rozpoczęcie transferu"
+DFT_ABORTED = u"transfer przerwany"
+DFT_COMPLETE = u"transfer ukończony"
+DFT_ERROR = u"błąd"
 
 #settings dialaog
 DSET_TITLE = u"Konfiguracja TorChat"
@@ -131,7 +131,7 @@ DSET_GUI_LANGUAGE = u"Język"
 DSET_GUI_OPEN_MAIN_HIDDEN = u"Uruchom w tle."
 DSET_GUI_OPEN_CHAT_HIDDEN = u"Nie otwieraj automatycznie nowych okien."
 DSET_GUI_NOTIFICATION_POPUP = u"Powiadamiający pop-up"
-# DSET_GUI_NOTIFICATION_METHOD = u"Notification method"
+DSET_GUI_NOTIFICATION_METHOD = u"Sposoby powiadomień"
 DSET_GUI_FLASH_WINDOW = u"Sygnalizuj nadejście nowej wiadomości"
 DSET_MISC_TITLE = u"Inne"
 DSET_MISC_TEMP_IN_DATA = u"Przechowuj pliki tymczasowe w katalogu danych"
@@ -142,23 +142,23 @@ NOTICE_DELAYED_MSG_WAITING = u"opóźnione wiadomości czekają w kolejce"
 NOTICE_DELAYED_MSG_SENT = u"opóźnione wiadomości zostały wysłane"
 NOTICE_DELAYED = u"opóźnione"
 
-# #messagebox for offline messages
-# MSG_OFFLINE_TITLE = u"TorChat: queued messages"
-# MSG_OFFLINE_EMPTY = u"there are no (more) queued messages for %s"
-# MSG_OFFLINE_QUEUED = u"queued offline messages for %s:\n\n%s"
+#messagebox for offline messages
+MSG_OFFLINE_TITLE = u"TorChat: oczekujące wiadomości"
+MSG_OFFLINE_EMPTY = u"nie ma (więcej) oczekujących wiadomości dla %s"
+MSG_OFFLINE_QUEUED = u"oczekujące wiadomości offline dla %s:\n\n%s"
 
-# #buddy list mouse hover popup
-# BPOP_BUDDY_IS_OFFLINE = u"Buddy is offline"
-# BPOP_CONNECTED_AWAITING_RETURN_CONN = u"Connected, awaiting return connection..."
-# BPOP_CLIENT_SOFTWARE = u"Client: %s %s"
+#buddy list mouse hover popup
+BPOP_BUDDY_IS_OFFLINE = u"Znajomy jest offline"
+BPOP_CONNECTED_AWAITING_RETURN_CONN = u"Połączony, oczekiwanie na połączenie zwrotne..."
+BPOP_CLIENT_SOFTWARE = u"Klient: %s %s"
 
-# #logging of conversations to file
+#logging of conversations to file
 # LOG_HEADER = u"This log file is not signed and has no cogency of proof"
-# LOG_STARTED = u"Logging started"
-# LOG_STOPPED = u"Logging stopped"
-# LOG_DELETED = u"Log files have been deleted"
-# LOG_IS_ACTIVATED = u"Logging to file is activated:\n%s"
-# LOG_IS_STOPPED_OLD_LOG_FOUND = u"Logging is stopped but old log file still exists:\n%s"
+LOG_STARTED = u"Rejestrowanie rozpoczęte"
+LOG_STOPPED = u"Rejestrowanie zakończone"
+LOG_DELETED = u"Pliki dziennika zostały usunięte"
+LOG_IS_ACTIVATED = u"Rejestrowanie do pliku zostało aktywowane:\n%s"
+LOG_IS_STOPPED_OLD_LOG_FOUND = u"Rejestrowanie zostało zatrzymane ale stare pliki dziennika wciąż istnieją:\n%s"
 
 #about box
 ABOUT_TITLE = u"O programie TorChat"
