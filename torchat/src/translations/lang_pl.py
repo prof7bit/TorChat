6b3bf8cb8f5ac7fd2b53028d7631515602f027c0
Szary Czlowiek <szary.czlowiek@hotmail.com>
2012-02-25 17:42:45 +0100

    small correction in the names of translators

--- torchat/src/translations/lang_pl.py
+++ torchat/src/translations/lang_pl.py
@@ -11,7 +11,7 @@
 LANGUAGE_CODE = u"pl"
 LANGUAGE_NAME = u"Polski"
 LANGUAGE_NAME_ENGLISH = u"Polish"
-TRANSLATOR_NAMES = [u"rmjy2avgeluvbrdq"]
+TRANSLATOR_NAMES = [u"yr3gwa7yonxrl26q", u"rmjy2avgeluvbrdq"]
 
 #buttons
 BTN_CANCEL = u"Anuluj"
