50517f99ad0b82bf1da6a0b2287f823824cc9bf4
prof7bit <prof7bit@googlemail.com>
2012-05-10 21:31:49 +0200

    documentation (TReceiver)

--- src/client/receiver.pas
+++ src/client/receiver.pas
@@ -17,6 +17,52 @@
   to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
   MA 02111-1307, USA.
 }
+
+{ This unit implements the receiver thread, each socket
+  connection has one of these threads sitting in a blocking
+  read on the socket. It is responsible for processing the
+  incoming data. It is also responsible for detecting when
+  the connection is closing/failing and triggering the
+  appropriate actions in this case.
+
+  Each protocol message is transported over the socket as
+  one long line of text, binary data is encoded, 0x0a
+  (line feed) marks the end of a message and the beginning
+  of the next. The first part of the message is a string
+  consisting of only the characters [a..z,_], separated by
+  a space. This is the command. The rest of the line is the
+  payload.
+
+  Processing incoming messages works like this:
+  * read the entire line from the socket until 0x0a is found.
+    (see also TReceiver (in receiver.pas) where this mechanism
+    is implemented.)
+  * separate the first word (delimited by space) from the rest
+    of the line. (The rest may also be empty, there exist
+    messages without payload.)
+  * instantiate the appropriate message class (one of the
+    TMsgXXX classes depending on what command it is) and feed
+    the rest of the line to its constructor. The constructor
+    will do the binary decoding of the payload. (The Paylod
+    is always encoded, no matter what it contains, so it will
+    also always be decoded. Please note that simple ascii
+    strings without '\' or 0x0a will be invariant under this
+    encoding, so it might look the same before and after,
+    this doesn't mean its not encoded. It is. Always.)
+  * call the message object's Parse() method, this will parse
+    the decoded payload (the payload after binary decoding is
+    one binary blob, depending on the type of the message it
+    might be somethig as simple as an UTF8 string or it might
+    be a chunk of a file transfer or something else, the Parse()
+    method is responsible for dissecting it and populating the
+    individual fields of the message object).
+  * enqueue the object into TorChat's message queue. The main
+    thread will pick it up from there and then call the
+    message's Execute() method. The Execute() method will do
+    the actual work, it contains the logic to decide what to
+    do, trigger responses, manipulate other objects, call the
+    GUI, etc.
+}
 unit receiver;
 
 {$mode objfpc}{$H+}
@@ -34,7 +80,9 @@ type
   { TReceiver - Each conection contains a TAReceiver object which is
     a thread reading from the blocking socket, splitting the received
     data into individual messages and for each of them instantiate the
-    appropriate message class to parse and enqueue the message }
+    appropriate message class to parse and enqueue the message. When
+    the connection is closing/failing it will notify its containing
+    connection object. See also THiddenConnection. }
   TReceiver = class(TAReceiver)
     constructor Create(AConn: TAHiddenConnection);
     destructor Destroy; override;
