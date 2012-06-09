9cdea43b1bfae65ad498da3a0a7041177ee895ea
prof7bit <prof7bit@googlemail.com>
2012-06-09 12:57:17 +0200

    add some needed purple_imgstore_* functions to purple.pas

--- src/purple/purple.pas
+++ src/purple/purple.pas
@@ -411,6 +411,9 @@ procedure purple_debug_warning(category: PChar; format: PChar; args: array of co
 procedure purple_debug_error(category: PChar; format: PChar; args: array of const); external LIBPURPLE;
 function  purple_find_buddies(account: PPurpleAccount; aname: PChar): PGSList; external LIBPURPLE;
 function  purple_find_buddy(account: PPurpleAccount; aname: PChar): PPurpleBuddy; external LIBPURPLE;
+function  purple_imgstore_get_data(img: PPurpleStoredImage): Pointer; external LIBPURPLE;
+function  purple_imgstore_get_extension(img: PPurpleStoredImage): PChar; external LIBPURPLE;
+function  purple_imgstore_get_size(img: PPurpleStoredImage): PtrUInt; external LIBPURPLE;
 function  purple_notify_message(Plugin: PPurplePlugin;
  typ: TPurpleNotifyMsgType; title: PChar; primary: PChar; secondary: PChar;
  cb: PPurpleNotifyCloseCallback; UserData: Pointer): GBoolean; external LIBPURPLE;
