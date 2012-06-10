fc1a4f89669e4b58a6392a2d27e2c2502fd538f6
prof7bit <prof7bit@googlemail.com>
2012-06-10 21:47:23 +0200

    bump version

--- src/client/tc_const.pas
+++ src/client/tc_const.pas
@@ -15,7 +15,7 @@ const
   SECONDS_FIRST_CONNECT = 5;     // first connect after TBuddy.Create
 
   SOFTWARE_NAME : String = 'TorChat';
-  SOFTWARE_VERSION : String = '2.0-alpha-5';
+  SOFTWARE_VERSION : String = '2.0-alpha-6';
 
 
 implementation
