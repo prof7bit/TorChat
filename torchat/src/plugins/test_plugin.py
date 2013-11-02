29fea2c01773e59aadef2a6129c42964af0d9aa4
Bitcoin Fan <ali.virod@yandex.ru>
2013-11-02 04:38:17 +0400

    change translation rules for plugins

--- torchat/src/plugins/test_plugin.py
+++ torchat/src/plugins/test_plugin.py
@@ -1,4 +1,7 @@
-NAME = u'Test plugin, replaces all sent messages with "test"'
+# -*- coding: UTF-8 -*-
+
+NAME_en = u'Test plugin, replaces all sent messages with "test"'
+NAME_ru = u'Проверочный плагин, заменяет все отправляемые сообщения на "test"'
 def load(torchat):
     sendChatMessage = torchat.tc_client.Buddy.sendChatMessage
     class Buddy(torchat.tc_client.Buddy):
--- torchat/src/torchat.py
+++ torchat/src/torchat.py
@@ -39,6 +39,7 @@ else:
         print "(2) wxversion screwed up, this is harmless, ignoring it."
 
 import wx
+import re
 import os
 import sys
 import imp
@@ -47,6 +48,7 @@ import tc_client
 import tc_gui
 import dlg
 import dlg_settings
+import translations
 
 CORE_MODULES = ('config', 'dlg', 'dlg_settings', 'tc_client', 'tc_gui')
 # translations are not restored, plugins should not change old members
@@ -58,18 +60,23 @@ def restore_orig_members():
     for module in CORE_MODULES:
         vars(__import__(module)).update(ORIG_MEMBERS[module])
 
-plugins = {} # shortname to python modules
-# plugin module must have NAME member (string, long name)
+# xx to module
+TRANSLATIONS = {}
+translations_dir = os.path.join(os.path.dirname(__file__), 'translations')
+for translation_file in os.listdir(translations_dir):
+    if re.match(r'^lang_..\.py$', translation_file):
+        translation_name = translation_file[:-3]
+        xx = translation_name[-2:]
+        translation_path = os.path.join(translations_dir, translation_file)
+        TRANSLATIONS[xx] = getattr(translations, translation_name)
+
+PLUGINS = {} # shortname to python modules
+# plugin module must have NAME_en member (string, long name)
 # plugin module must have load(torchat) member, torchat is this module
 # plugin module should have NAME_<LANG> with translation of name
 # plugins are searched in app's plugins/ dir and in getDataDir()/plugins
 # plugins with same name in getDataDir()/plugins win over app's plugins/ dir
-plugins_parent_dirs = []
-try:
-    plugins_parent_dirs.append(os.path.dirname(__file__))
-except:
-    pass
-plugins_parent_dirs.append(config.getDataDir())
+plugins_parent_dirs = [os.path.dirname(__file__), config.getDataDir()]
 for plugins_parent_dir in plugins_parent_dirs:
     plugins_dir = os.path.join(plugins_parent_dir, 'plugins')
     if os.path.exists(plugins_dir):
@@ -77,14 +84,19 @@ for plugins_parent_dir in plugins_parent_dirs:
             if plugin_file.endswith('.py'):
                 plugin_name = plugin_file[:-3]
                 plugin_path = os.path.join(plugins_dir, plugin_file)
-                plugins[plugin_name] = imp.load_source(plugin_name, plugin_path)
+                PLUGINS[plugin_name] = imp.load_source(plugin_name, plugin_path)
+                for xx in TRANSLATIONS:
+                    if hasattr(PLUGINS[plugin_name], 'NAME_' + xx):
+                        dscr = getattr(PLUGINS[plugin_name], 'NAME_' + xx)
+                        var_name = 'DSET_PLUGIN_' + plugin_name.upper()
+                        setattr(TRANSLATIONS[xx], var_name, dscr)
 
 def reload_plugins():
     restore_orig_members()
     enabled_plugins = config.get('plugin', 'enabled_plugins').split(',')
     for plugin_name in enabled_plugins:
-        if plugin_name in plugins:
-            plugins[plugin_name].load(sys.modules[__name__])
+        if plugin_name in PLUGINS:
+            PLUGINS[plugin_name].load(sys.modules[__name__])
 
 reload_plugins()
 
