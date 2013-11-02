76e780756a23b5ff56a90794723591607c4225f8
Bitcoin Fan <ali.virod@yandex.ru>
2013-11-02 18:30:46 +0400

    feature test_plugin
    *settings item for replacement text
    *translations

--- torchat/src/plugins/test_plugin.py
+++ torchat/src/plugins/test_plugin.py
@@ -2,10 +2,24 @@
 
 NAME_en = u'Test plugin, replaces all sent messages with "test"'
 NAME_ru = u'Проверочный плагин, заменяет все отправляемые сообщения на "test"'
+
 def load(torchat):
     _sendChatMessage = torchat.tc_client.Buddy.sendChatMessage
     def sendChatMessage(self, text):
-        text = u'test'
+        text = torchat.config.get("test_plugin", "text")
         _sendChatMessage(self, text)
     torchat.tc_client.Buddy.sendChatMessage = sendChatMessage
+
+    torchat.config.config_defaults['test_plugin', 'text'] = 'test'
+
+    torchat.TRANSLATIONS['en'].DSET_TEST_PLUGIN_TEXT = u'Replace any message with text'
+    torchat.TRANSLATIONS['ru'].DSET_TEST_PLUGIN_TEXT = u'Заменять сообщения текстом'
+    torchat.config.importLanguage()
+
+    _constructor = torchat.dlg_settings.Dialog.__init__
+    def constructor(self, main_window):
+        _constructor(self, main_window)
+        torchat.dlg.Text(self.p3, torchat.dlg_settings.lang.DSET_TEST_PLUGIN_TEXT,
+                ("test_plugin", "text"))
+    torchat.dlg_settings.Dialog.__init__ = constructor
 
