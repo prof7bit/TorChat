520b2a7a6223a22623878e1da28321883826c6db
Bitcoin Fan <ali.virod@yandex.ru>
2013-11-02 21:34:19 +0400

    conference !list command

--- torchat/src/plugins/conference.py
+++ torchat/src/plugins/conference.py
@@ -5,20 +5,31 @@ NAME_ru = u'Превращает аккаунт в конференцию'
 
 def load(torchat):
     _execute = torchat.tc_client.ProtocolMsg_message.execute
+    def nick_repr(buddy, nick_viewer):
+        nick = buddy.address
+        if int(torchat.config.get("conference", "prefer_nicks")) == 1 \
+                and buddy.profile_name \
+                and not torchat.tc_client.isValidAddress(buddy.profile_name):
+            nick = buddy.profile_name
+        return nick
     def execute(self):
         goood_message = self.buddy and self.buddy in self.bl.list
         if int(torchat.config.get("conference", "no_gui")) == 0 or not goood_message:
             _execute(self)
         if goood_message:
-            nick = self.buddy.address
-            if int(torchat.config.get("conference", "prefer_nicks")) == 1 \
-                    and self.buddy.profile_name \
-                    and not torchat.tc_client.isValidAddress(self.buddy.profile_name):
-                nick = self.buddy.profile_name
-            resent_message = '%s: %s' % (nick, self.text)
-            for buddy in self.bl.list:
-                if buddy != self.buddy and buddy != self.bl.own_buddy:
-                    buddy.sendChatMessage(resent_message)
+            if self.text.startswith('!'):
+                # command
+                if self.text == '!list':
+                    nicks = []
+                    for buddy in self.bl.list:
+                        nicks.append(nick_repr(buddy, self.buddy))
+                    self.buddy.sendChatMessage(' '.join(nicks))
+            else:
+                # resend message
+                for buddy in self.bl.list:
+                    resent_message = '%s: %s' % (nick_repr(self.buddy, buddy), self.text)
+                    if buddy != self.buddy and buddy != self.bl.own_buddy:
+                        buddy.sendChatMessage(resent_message)
     torchat.tc_client.ProtocolMsg_message.execute = execute
 
     torchat.config.config_defaults['conference', 'prefer_nicks'] = 1
